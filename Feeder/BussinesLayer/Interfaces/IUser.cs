77293728830b7038f40ef27c75177fdb12a8d553
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-12-02 15:52:13 +0300

    Create BussinesLayer

---
+++ Feeder/BussinesLayer/Interfaces/IUser.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using Entities.Feeders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Interfaces
+{
+    public interface IUser
+    {
+        Task AddFeeder(Feeder feeder);
+        Task AddMark(string mark);
+        Task CreateTimetable();
+        Task UpdateTimetable(string name); 
+        Task DeleteTimetable(string name);
+        Task Monitoring();
+        Task<IReadOnlyCollection<Log>> GetLogs();
+        Task ManualControl();
+    }
+}
